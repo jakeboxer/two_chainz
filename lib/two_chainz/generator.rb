c94e2751da168572dc8e6ab898617c75360ecedd
Jake Boxer <jake@github.com>
2013-05-11 19:51:21 -0700

    Get Generator#hear tests passing
    * Add Generator#words_table attr
    * Keep track of words heard during #hear

--- lib/two_chainz/generator.rb
+++ lib/two_chainz/generator.rb
@@ -1,10 +1,23 @@
 class TwoChainz::Generator
+  attr_accessor :words_table
+
+  def initialize
+    @words_table = {}
+  end
+
   # Public: Hear some words and remember them for future spitting.
   #
   # words - String of words to hear.
   #
-  # Returns the number of unique words the generator has heard (integer)
+  # Returns the number of unique new words the generator heard (integer)
   def hear(words)
-    0
+    heard = 0
+
+    words.scan(/[\w\']+/) do |word|
+      heard += 1 unless words_table[word]
+      words_table[word] = true
+    end
+
+    heard
   end
 end
