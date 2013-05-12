489bd511c2f7ee9d604833e7134c439ba8b2e1e9
Jake Boxer <jake@github.com>
2013-05-11 19:26:13 -0700

    Add base implementaton for Generator#hear

--- lib/two_chainz/generator.rb
+++ lib/two_chainz/generator.rb
@@ -1,2 +1,10 @@
 class TwoChainz::Generator
+  # Public: Hear some words and remember them for future spitting.
+  #
+  # words - String of words to hear.
+  #
+  # Returns the number of unique words the generator has heard (integer)
+  def hear(words)
+    0
+  end
 end
--- test/two_chainz/generator_test.rb
+++ test/two_chainz/generator_test.rb
@@ -1,9 +1,31 @@
 require 'test_helper'
 
 describe TwoChainz::Generator do
+  before do
+    @generator = TwoChainz::Generator.new
+  end
+
   describe 'new' do
     it 'must create a new instance of TwoChainz::Generator' do
-      refute_nil TwoChainz::Generator.new, "TwoChainz::Generator#new should create a new TwoChainz::Generator"
+      refute_nil @generator, "TwoChainz::Generator#new should create a new TwoChainz::Generator"
+    end
+  end
+
+  describe 'hear' do
+    it 'must return 0 when an empty string is passed' do
+      assert_equal 0, @generator.hear('')
+    end
+
+    it 'must return 1 when a one-word string is passed' do
+      assert_equal 1, @generator.hear('sup')
+    end
+
+    it 'must return 1 when a string with two of the same words are passed' do
+      assert_equal 1, @generator.hear('yo yo')
+    end
+
+    it 'must return 2 when a string with two different words are passed' do
+      assert_equal 2, @generator.hear('love you')
     end
   end
 end
