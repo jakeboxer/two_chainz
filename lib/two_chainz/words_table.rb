d8e92908d87fd4808961c5fae24944982c4681f9
Jake Boxer <jake@github.com>
2013-05-19 16:37:52 -0700

    Add Generator#increment and #include?

--- lib/two_chainz/words_table.rb
+++ lib/two_chainz/words_table.rb
@@ -1,2 +1,39 @@
 class TwoChainz::WordsTable
+  # Public: Create a new TwoChainz::WordsTable instance.
+  #
+  # Returns a TwoChainz::WordsTable
+  def initialize
+    # The table is a hash. Think of its entries as "rows".
+    # Each row is another hash. Think of its entries as "cells".
+    # Cells default to 0.
+    @table = {}
+  end
+
+  # Public: Increment the value in the table for the specified pair of words.
+  #
+  # first_word  - The word that comes first in the pair. If this is nil, the
+  #               second word in the pair will be recorded as the beginning of
+  #               a sentence.
+  # second_word - The word that comes last in the pair. If this is nil or
+  #               omitted, the first word in the pair will be recorded as the
+  #               ending of a sentence.
+  #
+  # Returns the new count of the entry.
+  def increment(first_word=:beginning, second_word=:ending)
+    if first_word == :beginning && second_word == :ending
+      raise ArgumentError, "At least one word must be provided to TwoChainz::WordsTable#increment"
+    end
+
+    @table[first_word]              ||= Hash.new(0)
+    @table[first_word][second_word]  += 1
+  end
+
+  # Public: Whether or not the words table has the specified word.
+  #
+  # word - Word to check for inclusion of.
+  #
+  # Returns a boolean
+  def include?(word)
+    @table.keys.include?(word)
+  end
 end
--- test/two_chainz/words_table_test.rb
+++ test/two_chainz/words_table_test.rb
@@ -4,4 +4,43 @@ describe TwoChainz::WordsTable do
   before do
     @table = TwoChainz::WordsTable.new
   end
+
+  describe 'increment' do
+    it 'must error if there are no arguments' do
+      assert_raises(ArgumentError) { @table.increment }
+    end
+
+    it 'must return the correct value when there is one argument' do
+      assert_equal 1, @table.increment('chainz')
+      assert_equal 2, @table.increment('chainz')
+    end
+
+    describe 'when there are two arguments' do
+      it 'must return the correct value when the first argument is nil' do
+        assert_equal 1, @table.increment(nil, 'chainz')
+        assert_equal 2, @table.increment(nil, 'chainz')
+      end
+
+      it 'must return the correct value when the second argument is nil' do
+        assert_equal 1, @table.increment('chainz', nil)
+        assert_equal 2, @table.increment('chainz', nil)
+      end
+
+      it 'must return the correct value when neither argument is nil' do
+        assert_equal 1, @table.increment('two', 'chainz')
+        assert_equal 2, @table.increment('two', 'chainz')
+      end
+    end
+  end
+
+  describe 'include?' do
+    it 'must be true if the word is in the table' do
+      @table.increment('chainz')
+      assert @table.include?('chainz')
+    end
+
+    it 'must be false if the word is not in the table' do
+      refute @table.include?('chainz')
+    end
+  end
 end
